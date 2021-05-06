8f42cb3e0b2357e1f24884f2d70ec1e8f29ec13d
Michael Ruane <mruane@dvrpc.org>
2021-05-05 22:23:40 -0400

    Update navbar to semantic elements

--- components/layout/NavBar.vue
+++ components/layout/NavBar.vue
@@ -1,56 +1,57 @@
 <template>
-    <header class="">
-        <nav class="top-0 absolute w-full flex flex-wrap items-center justify-between border-t-8 border-b border-purple bg-gradient-to-r from-purple via-primary to-primary">
-            <div class="container mx-auto flex flex-wrap items-center justify-between ">
-                <div id="logos" class="">
-                    <div class="hidden sm:inline-block">
-                        <DvrpcLogo class="h-14"/>
-                    </div>
-                    <Logo class="h-14" />
-                    <button
-                    class="cursor-pointer text-xl leading-none px-3 py-1 rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                    type="button"
-                    v-on:click="toggleNavbar()"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" style="fill: #fff"><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/></svg>
-                    </button>
+    <header class="top-0 absolute w-full flex flex-wrap items-center justify-between border-t-8 border-b border-purple bg-gradient-to-r from-purple via-primary to-primary">
+        <div class="container mx-auto flex flex-wrap items-center justify-between ">
+            <!-- brand content -->
+            <div id="logos" class="">
+                <div class="hidden sm:inline-block">
+                    <DvrpcLogo class="h-14"/>
                 </div>
-                <div class="lg:flex flex-grow items-center"
-                    :class="{'hidden': !showMenu, 'block': showMenu}">
-                    <ul class="flex flex-col lg:flex-row list-none ml-auto">
-                        <li class="nav-item">
-                            <a 
-                                class="" href="#pablo"
-                            >
-                                Home
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a 
-                                class="" href="#pablo"
-                            >
-                                About
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a 
-                                class="" href="#pablo"
-                            >
-                                Tools
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a 
-                                class="" href="#pablo"
-                            >
-                                View Map
-                            </a>
-                        </li>
-                        
-                    </ul>
-                </div>
+                <Logo class="h-14" />
+                <button
+                class="cursor-pointer text-xl leading-none px-3 py-1 rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                type="button"
+                v-on:click="toggleNavbar()"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" style="fill: #fff"><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/></svg>
+                </button>
             </div>
-        </nav>
+            <!-- begin nav elements -->
+            <nav 
+            class="lg:flex flex-grow items-center"
+            :class="{'hidden': !showMenu, 'block': showMenu}"
+            >
+                <ul class="flex flex-col lg:flex-row list-none ml-auto">
+                    <li class="nav-item">
+                        <a 
+                            class="" href="#pablo"
+                        >
+                            Home
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a 
+                            class="" href="#pablo"
+                        >
+                            About
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a 
+                            class="" href="#pablo"
+                        >
+                            Tools
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a 
+                            class="" href="#pablo"
+                        >
+                            View Map
+                        </a>
+                    </li> 
+                </ul>
+            </nav>
+        </div>
     </header>
 </template>
 
